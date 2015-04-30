e078cdd7534a4fcf398e9dc5d829b7d99a787a89
Sunbrye Ly <suly@ucsd.edu>
2015-04-30 15:19:34 -0700

    d3 c3 work

--- public/js/c3visualization.js
+++ public/js/c3visualization.js
@@ -4,16 +4,20 @@
       var yCounts = data.users.map(function(item){
         return item.counts.media;
       });
-      
-      yCounts.unshift('Media Count');
 
-      var chart = c3.generate({
-        bindto: '#chart',
-        data: {
-          columns: [
-            yCounts 
-          ]
-        }
-      });
-    });
+       yCounts.unshift('User Follows');
+
+var chart = c3.generate({
+    data: {
+       // xs: {
+         // Follows: 'Follows_x',         
+       // },
+        columns: 
+            [yCounts],
+      type: "scatter"
+    }
+});
+
+
+   });
 })();
